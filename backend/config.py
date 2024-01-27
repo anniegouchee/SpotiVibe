4d321fe67f387c354244d020e538ceb34865514a
Luke Wang <lukewang63@gmail.com>
2024-01-27 17:15:42 -0500

    Added other files to .gitignore

---
+++ backend/config.py
@@ -0,0 +1,3 @@
+FACE_DETECTION_XML = "haarcascade_frontalface_default.xml"
+MODEL_COMPILATION = "mod_empathly_model_94p24.h5"
+SPOTIFY_TOKEN = "BQDtpH5qpsTKYMz_4N11TMsbrr2e1_L4lmLGOEcDyLZia5_r3IeHPBExqet8DtHhX6Vwvi5k6jgawHqtSp8Ax5CDj-                                sartjszjbKwuFibwHWqdI1gitqN2xh2Xk2KAmZoO03Q0YNXtPVOJ9iZsPqIiV9FRDRBUYx9TCE0XZxiXoFeG9terbQB1SqVO-                                          TBjXhzaP4Xwb_9btpGwGPvtuEso6I3Ricq3SOrkuImGD2_VQKZZ8rUSFY1T8kLluKT0L-SZWUP7niXjpDOXW0la_EeaKI"
