eb21aad500aeadb42cd6e6813778fcbf4ecaec7d
Annie <annie.gouchee@mail.mcgill.ca>
2024-01-28 09:54:28 -0500

    routes added

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,4 +1,6 @@
 import React,{ useState, useEffect} from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import './App.css';
 import WebCamPage from "./components/WebCamPage";
 import SpotifyAuth from "./components/spotifyAuth";
@@ -6,21 +8,28 @@ import io from 'socket.io-client';
 
 function App() {
   const [socket, setSocket] = useState(null);
-  const [accessToken, setAccessToken] = useState('');
+  
+  // const [accessToken, setAccessToken] = useState('');
 
-  useEffect(() => {
-      const newSocket = io.connect('http://localhost:4999');
-      setSocket(newSocket);
-      return () => newSocket.disconnect();
-  }, [accessToken]);
+  // useEffect(() => {
+  //     const newSocket = io.connect('http://localhost:4999');
+  //     setSocket(newSocket);
+  //     return () => newSocket.disconnect();
+  // }, [accessToken]);
 
   return (
-    <div>
-      {accessToken ? 
-        (<WebCamPage socket={socket} setAccessToken={setAccessToken}/>) : 
-        (<SpotifyAuth socket={socket} setAccessToken={setAccessToken} />)
-      }
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginSignUp />} />
+        <Route path="/home" element={<WebCamPage />} />
+      </Routes>
+    </Router>
+    // <div>
+    //   {accessToken ? 
+    //     (<WebCamPage socket={socket} setAccessToken={setAccessToken}/>) : 
+    //     (<SpotifyAuth socket={socket} setAccessToken={setAccessToken} />)
+    //   }
+    // </div>
   );
 }
 
