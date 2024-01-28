2832bf4b049b07a2bbf0ea8a284fd553db1a8f55
Luke Wang <lukewang63@gmail.com>
2024-01-28 09:30:00 -0500

    Fixed mood_dict

--- backend/app.py
+++ backend/app.py
@@ -9,7 +9,7 @@ import config
 FACE_DETECTION_XML = config.FACE_DETECTION_XML
 MODEL_COMPILATION = tf.keras.models.load_model(config.MODEL_COMPILATION)
 global_spotify_token = config.SPOTIFY_TOKEN
-mood_dict = ["angry", "tired", "fear", "happy", "sad", "excited", "neutral"]
+mood_dict = ["angry", "excited", "happy", "neutral", "sad", "tired"]
 
 mood_history = {
     "angry": 0,
@@ -44,8 +44,8 @@ def detect_mood(frame):
         Predictions = MODEL_COMPILATION.predict(final_image)
 
         status = mood_dict[np.argmax(Predictions)]
-        recommended_songs = recommend_songs_for_mood(status, global_spotify_token)
-        return status, recommended_songs
+        # recommended_songs = recommend_songs_for_mood(status, global_spotify_token)
+        return status  # , recommended_songs
 
 
 app = Flask(__name__, static_folder="./templates/static")
@@ -81,7 +81,8 @@ def receive_image(image):
     # Decode the base64-encoded image data
     image = base64_to_image(image)
 
-    mood, recommended_songs = detect_mood(image)
+    # mood, recommended_songs = detect_mood(image)
+    mood = detect_mood(image)
     if mood:
         mood_history[mood] += 1
         mood_history["total"] += 1
@@ -99,11 +100,12 @@ def receive_image(image):
             if key == "total":
                 continue
             mood_percentage[key] = mood_history[key] / total
+        print(mood_percentage)
         emit(
             "analysis_result",
             {
                 "mood": mood,
-                "songs": recommended_songs,
+                # "songs": recommended_songs,
                 "moodPercentage": mood_percentage,
             },
         )
